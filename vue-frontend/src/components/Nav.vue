c9c13ca509bfcc60cecf2d14f03f87f261055654
Szymon Kilian <szymonkilian98@o2.pl>
2023-09-11 16:51:53 +0200

    Install Breeze SPA authentication, Basic Routing

---
+++ vue-frontend/src/components/Nav.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="navbar">
+      <router-link :to="{ name: 'Home' }" class="nav-button home-button">Home</router-link>
+      <router-link :to="{ name: 'Login' }" class="nav-button login-button">Login</router-link>
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style>
+  .navbar {
+    background-color: #662549; /* Set the background color */
+    border-radius: 0 0 15px 15px; /* Rounded bottom corners */
+    display: flex;
+    justify-content: space-between; /* Push the buttons to the corners */
+    padding: 10px;
+  }
+  
+  .nav-button {
+    padding: 10px 20px; /* Add padding to create a button-like appearance */
+    color: white;
+    text-decoration: none;
+    border-radius: 5px; /* Rounded corners for the buttons */
+    transition: background-color 0.3s ease; /* Smooth transition for hover effect */
+  
+  }
+  
+  .home-button {
+    background-color: #451952; /* Button color for Home */
+  }
+  
+  .login-button {
+    background-color: #AE445A; /* Button color for Login */
+  }
+  
+  /* Hover effect */
+  .nav-button:hover {
+    background-color: #F39F5A;
+  }
+  </style>
+  
